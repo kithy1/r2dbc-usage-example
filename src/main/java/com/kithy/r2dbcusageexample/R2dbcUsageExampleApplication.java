43b3f36ea272b7423c04b290227a85042d250064
kithy1 <kithy@interia.eu>
2021-01-30 10:45:25 +0100

    Initial commit

---
+++ src/main/java/com/kithy/r2dbcusageexample/R2dbcUsageExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kithy.r2dbcusageexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcUsageExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcUsageExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kithy/r2dbcusageexample/R2dbcUsageExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kithy.r2dbcusageexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcUsageExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
